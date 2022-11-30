39fcfe5c63132f89f354146327c2cdc725bb8d6b
Yawi-i <yael.mattmann@gmail.com>
2022-11-30 15:05:41 +0000

    Modified Student and StudentTest

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -12,10 +12,29 @@ public class Student {
 
     public int Name(){
         int length = studentName.length();
-        if (length < 3){
+        if (length < 2){
             throw new IllegalArgumentException("Name should be 3 letters or more");
         }
         return length;
     }
 
+    public int Phone(){
+        int length = studentPhone.length();
+        if (length < 7){
+            throw new IllegalArgumentException("Phone should be 10 numbers or more");
+        }
+        return length;
+    }
+
+    public int Age(){
+        if (studentAge < 16){
+            throw new IllegalArgumentException("Age must be over 16");
+        }
+        return studentAge;
+    }
+
+    public String Email(){
+
+        return studentemail;
+    }
 }
--- src/test/java/ie/atu/StudentTest.java
+++ src/test/java/ie/atu/StudentTest.java
@@ -17,7 +17,7 @@ class StudentTest {
     @Test
     void TestNameLength(){
         myStudent = new Student("Ye", "phone", "mail", 0);
-        assertTrue(Student.Name() >= 2, "Name should be 2 letters or more");
+        assertTrue(myStudent.Name() >=2,"Name should be 2 letters or more");
     }
 
     @Test
@@ -27,6 +27,46 @@ class StudentTest {
                 () -> {
                     myStudent.Name();
                 });
+    }
+
+    @Test
+    void TestPhoneLength(){
+        myStudent = new Student("name", "0123456789", "mail", 0);
+        assertTrue(myStudent.Phone() >= 7,"Phone should be 7 characters or more");
+    }
+
+    @Test
+    void TestPhoneLengthFail(){
+        myStudent = new Student("name", "1234", "mail", 0);
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    myStudent.Phone();
+                });
+    }
+
+    @Test
+    void TestAgePass() {
+        myStudent = new Student("name", "phone", "mail", 16);
+        assertTrue(myStudent.Age() >= 16, "Age must be over 16");
+    }
+
+    @Test
+    void TestAgeFail() {
+        myStudent = new Student("name", "phone", "mail", 0);
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    myStudent.Age();
+                });
+    }
+
+    @Test
+    void TestEmailPass(){
+        myStudent = new Student("name", "phone", "mail@mail.ie", 0);
+    }
+
+    @Test
+    void TestEmailFail(){
+        myStudent = new Student("name", "phone", "iamasleep.idk", 0);
     }
 
 
