d2c887ffc06c7bd8183705dfe9b359cc9da92641
Yael <yael.mattmann@gmail.com>
2022-12-13 22:58:45 +0000

    Modified a test parameter for the demonstration of the pipeline

--- src/test/java/ie/atu/StudentTest.java
+++ src/test/java/ie/atu/StudentTest.java
@@ -39,7 +39,7 @@ public class StudentTest {
     void TestPhoneLengthFail(){
         assertThrows(IllegalArgumentException.class,
                 () -> {
-                    myStudent.setPhone("012345678");
+                    myStudent.setPhone("012345687");
                 });
     }
 
