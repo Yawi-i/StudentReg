31ff0b7010116e494ad80e22a957dc3eb7ed2423
Johnson <G00389668@atu.ie>
2022-12-13 16:02:06 +0000

    added new class called payment & new test class called PaymentTest. Added methods to payment

---
+++ src/test/java/ie/atu/PaymentTest.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
