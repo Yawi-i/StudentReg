511ec4831395eb1fa0cf8d0a8ec947acf5cc58b5
Yawi-i <yael.mattmann@gmail.com>
2022-12-14 11:42:22 +0000

    StudID

--- src/main/java/ie/atu/StudID.java
+++ src/main/java/ie/atu/StudID.java
@@ -1,5 +1,37 @@
 package ie.atu;
 
 public class StudID {
+    //String studentID, studentReg;
+    private String ID;
+    private String Reg;
 
+
+    public StudID(String id, String reg){      //Constructor
+         Reg = reg;
+         ID = id;
+    }
+
+    public void setID(String id) {
+        if (String.valueOf(ID).length() > 7) {
+            ID = id;
+        } else {
+            throw new IllegalArgumentException("Student ID number should be 8 numbers long. Please re- enter ID");
+        }
+    }
+
+    public void setReg(String reg) {
+        if (String.valueOf(reg).length() > 8) {
+            Reg = reg;
+        } else {
+            throw new IllegalArgumentException("Registration number should be 9 numbers long. Please re- enter ID");
+        }
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public String getReg(){
+        return Reg;
+    }
 }
--- src/test/java/ie/atu/StudIDTest.java
+++ src/test/java/ie/atu/StudIDTest.java
@@ -1,14 +1,37 @@
 package ie.atu;
 
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudIDTest {
+public class StudIDTest {
+
+    StudID myStudID;
 
     @BeforeEach
     void setUp() {
+        myStudID = new StudID("0012345","0012345");
+    }
+
+/*
+    @Test
+    public void testID(){
+        myStudID.setID("0012345");
+        assertEquals("0012345", myStudID.getID());
+    }
+*/
+    @Test
+    public void TestIDLength(){
+        myStudID.setID("5012345");
+        assertTrue(String.valueOf(myStudID.getID()).length() >= 7,"Student ID number should be 8 numbers long. Please re- enter ID");
+    }
+
+    @Test
+    public void testreg(){
+        myStudID.setReg("501234568");
+        assertTrue(String.valueOf(myStudID.getReg()).length() >= 9,"Registration number should be 9 numbers long. Please re- enter ID");
     }
 
     @AfterEach
